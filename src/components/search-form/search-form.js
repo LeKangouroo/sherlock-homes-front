a2646b10930684703cc1d41167f0f2f17afc0c2a
Itadakimas <said.ahemt@gmail.com>
2017-03-15 23:31:32 +0100

    adds zip codes tags

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,7 +1,16 @@
+import Taggle from 'taggle';
+
 export default {
 
+  data() {
+
+    return {
+      taggle: null
+    };
+  },
   mounted() {
 
+    this.taggle = new Taggle('zipCodesTags');
     console.log('search-form component mounted');
   }
 };
