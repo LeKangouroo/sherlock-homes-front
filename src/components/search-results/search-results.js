541c8f4ea875e86f0d563308bab7858d619586e2
Itadakimas <said.ahemt@gmail.com>
2017-03-16 23:57:09 +0100

    adds search results templating

--- src/components/search-results/search-results.js
+++ src/components/search-results/search-results.js
@@ -1,7 +1,16 @@
+import SearchStore from 'classes/search-store';
+
 export default {
 
+  data() {
+
+    return {
+      offers: []
+    };
+  },
   mounted() {
 
     console.log('search-results component mounted');
+    SearchStore.addObserver('results:change', (results) => this.offers = results);
   }
 };
